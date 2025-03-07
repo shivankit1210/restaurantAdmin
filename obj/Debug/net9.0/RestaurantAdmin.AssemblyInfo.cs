d54d5a661173cfb88721c9700653b239acbfd18b
Shivankit Agarwal <shivankit1210@gmail.com>
2025-03-07 18:15:50 +0530

    Update API code

--- obj/Debug/net9.0/RestaurantAdmin.AssemblyInfo.cs
+++ obj/Debug/net9.0/RestaurantAdmin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09804d0b73f39bcbaac3d22da15f418526010215")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0128f76d6ad0b083671419ababb4eb9bf7a28b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
